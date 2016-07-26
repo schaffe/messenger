95a0b1de4ad5cfe15ef9b79f6db5942e98b0e677
Denys Konakhevych <denys.konakhevich@themidnightcoders.com>
2016-07-26 18:51:56 +0300

    initial commit

---
+++ src/main/java/com/messenger/Main.java
@@ -0,0 +1,15 @@
+package com.messenger;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: Denys Konakhevych
+ * Date: 26.07.2016
+ * Time: 18:45
+ */
+public class Main
+{
+  public static void main( String[] args )
+  {
+    System.out.println("Hello world!");
+  }
+}
